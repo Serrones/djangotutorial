1a588392fcbac66d371e998a402d0d1e79478dc7
Serrones <fabioserrones@gmail.com>
2016-10-09 17:55:29 -0300

    Last Project

--- myproject/core/views.py
+++ myproject/core/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
+#def home(request):
+#        return HttpResponse('<h1>Django</h1>'+'<h3>Bem-vindo ao .NET Coders, bitch!</h3>')
 # Create your views here.
+
+def home(request):
+    return render(request, 'index.html')
